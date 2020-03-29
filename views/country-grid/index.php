b75aeb898535548e80ab3f8de7b7df13fd30caeb
OpmaxSanford <62840757+OpmaxSanford@users.noreply.github.com>
2020-03-29 13:04:55 -0400

    Country Grid Generation

---
+++ views/country-grid/index.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\CountryGridSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Country Grids';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="country-grid-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Country Grid', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'code',
+            'name',
+            'population',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+
+</div>
