8171c09499eb46ad42bb879170e48d56baf8e109
Tony Inciarte <tonyinciarte@localhost.localdomain>
2020-04-21 14:28:07 -0400

    JS Test

--- views/jstest/index.php
+++ views/jstest/index.php
@@ -5,4 +5,22 @@ use yii\widgets\LinkPager;
 <h1>JS Test</h1>
   <script>
     console.log("Hello World!");
+
+    var dogs = {
+        Fido: "Mutt",  Hunter: "Doberman",  Snoopie: "Beagle"
+    };
+    var myDog = "Hunter";
+    var myBreed = dogs[myDog];
+    console.log(myBreed); // "Doberman"
+
+    var someObj = {
+      propName: "John"
+    };
+    function propPrefix(str) {
+      var s = "prop";
+      return s + str;
+    }
+    var someProp = propPrefix("Name"); // someProp now holds the value 'propName'
+    console.log(someObj[someProp]); // "John"
+
   </script>
